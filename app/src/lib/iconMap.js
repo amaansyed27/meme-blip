daa4b4be0d85635a895f0d59f6f8a2c8d877ba6b
Amaan Syed <amaansyed27@gmail.com>
2026-07-05 02:07:14 +0530

    Add icon map

---
+++ app/src/lib/iconMap.js
@@ -0,0 +1,13 @@
+import { Bell, BriefcaseBusiness, Gamepad2, Gauge, Laugh, Megaphone, Radio, Settings, Zap } from 'lucide-react';
+
+export const iconMap = {
+  bell: Bell,
+  dashboard: Gauge,
+  gamepad: Gamepad2,
+  laugh: Laugh,
+  megaphone: Megaphone,
+  radio: Radio,
+  settings: Settings,
+  briefcase: BriefcaseBusiness,
+  zap: Zap
+};
