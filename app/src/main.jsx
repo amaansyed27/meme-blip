403f9260f5553273e45d94a40d199001b386819d
Amaan Syed <amaansyed27@gmail.com>
2026-07-05 02:06:28 +0530

    Add React dashboard entrypoint

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,1 +1,6 @@
-console.log('hello');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './styles/global.css';
+
+createRoot(document.querySelector('#root')).render(<App />);
