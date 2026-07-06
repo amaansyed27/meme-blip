6aae34d7621ec03814d52fc2164201aaa54d34f9
Amaan Syed <amaansyed27@gmail.com>
2026-07-07 00:28:50 +0530

    Load board product styles

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -4,5 +4,6 @@ import App from './App.jsx';
 import './styles/global.css';
 import './styles/product.css';
 import './styles/hotkeys.css';
+import './styles/boards.css';
 
 createRoot(document.querySelector('#root')).render(<App />);
