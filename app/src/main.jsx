39b45f20694e484a2273dee78cbf6f53a9726c55
Amaan Syed <amaansyed27@gmail.com>
2026-07-07 03:18:24 +0530

    Load split product styles

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles/global.css';
-import './styles/product.css';
+import './styles/theme.css';
+import './styles/layout.css';
+import './styles/sounds.css';
+import './styles/routing.css';
 import './styles/hotkeys.css';
-import './styles/boards.css';
+import './styles/product.css';
 
 createRoot(document.querySelector('#root')).render(<App />);
