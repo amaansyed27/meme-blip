a8943dbc54ef5b1f0757ef1256a6a30ea97fd651
Amaan Syed <amaansyed27@gmail.com>
2026-07-06 19:39:17 +0530

    Load hotkey UI styles

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles/global.css';
 import './styles/product.css';
+import './styles/hotkeys.css';
 
 createRoot(document.querySelector('#root')).render(<App />);
